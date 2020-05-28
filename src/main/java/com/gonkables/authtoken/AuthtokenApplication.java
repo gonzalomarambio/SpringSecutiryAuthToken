763f7ef509d507e33ea8db1d7ab35fdb2c397e75
Gonzalo Marambio <gonzalo.marambio.gutierrez@gmail.com>
2020-05-28 18:38:11 -0400

    Base

---
+++ src/main/java/com/gonkables/authtoken/AuthtokenApplication.java
@@ -0,0 +1,13 @@
+package com.gonkables.authtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthtokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthtokenApplication.class, args);
+    }
+
+}
